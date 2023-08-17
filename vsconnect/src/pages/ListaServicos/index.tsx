b8d36d965a78466833fba1fb4a9b5985c600efbb
alanmmartins <alanmunozmartins@gmail.com>
2023-08-17 16:00:05 -0300

    Api Lista servico

--- vsconnect/src/pages/ListaServicos/index.tsx
+++ vsconnect/src/pages/ListaServicos/index.tsx
@@ -1,66 +1,72 @@
 import CardServico from "../../components/CardServicos";
 import "./style.css";
-import { useState } from "react";
+import { useState , useEffect} from "react";
+
+//
+import api from "../../utils/api";
 
 function ListaServicos() {
 
-    const[lista] =  useState<any[]>([
-        {
-            titulo: "Desenvolvimento de site institucional - Gateway de Pagamento / Fintech",
-            descricao: "Desenvolver um site responsivo que seja utilizado como uma plataforma de apresentação do nosso gateway de pagamento. O objetivo principal deste projeto é criar um site atraente e informativo, que demonstre as funcionalidades e benefícios do nosso gateway de pagamento para potenciais clientes.",
-            proposta: 1300,
-            techs: [
-              "HTML",
-              "CSS",
-              "REACT"
-            ]
-          },
-          {
-            titulo: "Bot telegram Pagamento",
-            descricao: "Preciso fazer um código em python para um bot do telegram. O bot será para solicitação de pagamento.",
-            proposta: 2400,
-            techs: [
-              "PYTHON"
-            ]
-          },
-          {
-            titulo: "Caixa Rápido",
-            descricao: "Preciso fazer um software que permita ao usuário fazer o upload de seu extrato bancário em formato( ofx). Dentro do software o mesmo poderá categorizar todas as suas receitas e despesas, tendo categorias sugeridas pelo software e permitindo também personalizações. Após o lançamento de vários extratos o software irá entender que são lançamentos parecidos e fará a categorização de maneira automática, cabendo ao usuário somente categorizar as receitas e despesas que não se repetem. Após a categorização o software irá emitir gráficos e relatórios baseados na categorização das contas.",
-            proposta: 1200,
-            techs: [
-              "PYTHON"
-            ]
-          }
+    const [servicos,setServicos] = useState<any[]>([
+
     ]);
-    const [listaServicosfiltrado, setListaServicosFiltrados] = useState<any[]>(lista);
+
+   /*  const [listaServicosfiltrado, setListaServicosFiltrados] = useState<any[]>(lista); */
 
     const [servicoDigitado, setServicoDigitado] = useState<string>("");
-    //função onde pega o que o usuario digitou
+
     function verificarCampoServico(event: any) {
         if (event.target.value === "") {
-            setListaServicosFiltrados(lista);
+            listarServicos();//retorno api
+
         }
         setServicoDigitado(event.target.value);
         console.log(servicoDigitado);
-        
+
     }
 
-        function buscarServicoPorSkill(event: any) {
-        //não recarrega a pagina
+    function buscarServicoPorSkill(event: any) {
+
         event.preventDefault();
 
-        //filtrar servicoss pela skill digitada no campo buscar
-        const servicosFiltrado = lista.filter((servico: any) => servico.techs.includes(servicoDigitado.toLocaleUpperCase()));
+        const servicosFiltrado = servicos.filter((servico: any) => servico.techs.includes(servicoDigitado.toLocaleUpperCase()));
 
         if (servicosFiltrado.length === 0) {
             alert("Nenhum desenvolvedor(a) com essa skill :(")
         } else {
-            //atribui valor de servicoss filtrado, ao state ListaservicosFiltrado 
-            setListaServicosFiltrados(servicosFiltrado);
+
+            /* setListaServicosFiltrados(servicosFiltrado); */
+            setServicos(servicosFiltrado)
+
         }
 
+    }
+
+
+    //obter servicos da api
+
+    function listarServicos() {
+        api.get("servicos")
+            .then((response: any) => {
+                console.log(response);
+                setServicos(response.data)
+            })
+
+            .catch((error: any) => {
+                console.log("Error ao realizar uma requisição servoco", error);
+
+            })
 
     }
+
+
+    //monitorar componente - só sera executado dps que a tela carregar
+    useEffect(() => {
+        //executa ação
+        listarServicos();
+
+    }, []);
+
     return (
         <>
             <main id="main_listaservicos">
@@ -72,29 +78,29 @@ function ListaServicos() {
                             <div className="wrapper_form">
                                 <label htmlFor="busca">Procurar serviços</label>
                                 <div className="campo-label">
-                                    <input onChange={verificarCampoServico} type="search" 
-                                    name="campo-busca" 
-                                    id="busca" 
-                                    placeholder="Buscar serviços por tecnologias..." />
+                                    <input onChange={verificarCampoServico} type="search"
+                                        name="campo-busca"
+                                        id="busca"
+                                        placeholder="Buscar serviços por tecnologias..." />
                                     <button type="submit">Buscar</button>
                                 </div>
                             </div>
                         </form>
                         <div className="wrapper_lista">
                             <ul>
-                            {
-                                    listaServicosfiltrado.map((servicos: any, indice: number) => {
+                                {
+                                    servicos.map((servicos: any, indice: number) => {
                                         return <li key={indice}>
                                             <CardServico
-                                            
-                                            titulo={servicos.titulo}
-                                            descricao={servicos.descricao}
-                                            proposta={servicos.proposta}
-                                            techs={servicos.techs}/>
+
+                                                titulo={servicos.titulo}
+                                                descricao={servicos.descricao}
+                                                proposta={servicos.proposta}
+                                                techs={servicos.techs} />
                                         </li>
                                     })
                                 }
-                             
+
                             </ul>
                         </div>
                     </div>
