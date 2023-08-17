a108e2075c02b58a644271da009d2e760bfecddc
alanmmartins <alanmunozmartins@gmail.com>
2023-08-17 13:02:41 -0300

    QA in Api

--- vsconnect/src/pages/ListaDevs/index.tsx
+++ vsconnect/src/pages/ListaDevs/index.tsx
@@ -1,110 +1,125 @@
-//estilização
-import "./style.css";
+import "./style.css"
 
-//components
 import CardDev from "../../components/CardDev";
 
-//hooks
 import { useEffect, useState } from "react";
 
 import api from "../../utils/api";
 
+
+//Hooks são funções JavaScript que permitem que você use recursos do React
+// state é como uma “variável” dentro de um componente.
+// useState é um hook que permite ao componente funcional ter um estado / ele serve para criar essa “variável”.
+// setState é para mudar o valor do estado. mudar o valor da “variável”.
+
+
 function ListaDevs() {
 
-    //STATE DEVS
-    const [devs, setDevs] = useState<any[]>([]);
+  // state devs
+  const [devs, setDevs] = useState<any[]>([]);
 
-    /* const [listaDevsFiltrados, setListaDevsFiltrados] = useState<any[]>(devs); */
+  // const [listaDevsFiltrados, setListaDevsFiltrados] = useState<any[]>(devs);
 
-    const [skillDigitado, setSkillDigitado] = useState<string>("");
+  const [skillDigitado, setSkillDigitado] = useState<string>("");
 
-    //função onde pega o que o usuario digitou
-    function verificarCampoSkill(event: any) {
-        if (event.target.value === "") {
-            /* setListaDevsFiltrados(devs); */
-            listarDesenvolvedores();
-        }
-        setSkillDigitado(event.target.value);
-    }
-
-    function buscarDevPorSkill(event: any) {
-        //não recarrega a pagina
-        event.preventDefault();
-
-        //filtrar devs pela skill digitada no campo buscar
-        const devsFiltrados = devs.filter((dev: any) => dev.hardskills.includes(skillDigitado.toLocaleUpperCase()));
-
-        if (devsFiltrados.length === 0) {
-            alert("Nenhum servico(a) com essa skill :(")
-        } else {
-            //atribui valor de devs filtrado, ao state ListaDevsFiltrados
-            setDevs(devsFiltrados);
-        }
-
-        }
-
-    function listarDesenvolvedores() {
-        api.get('users')
-        .then((response:any)=>{
-            console.log(response);
-            
-            //setDevs(response.data)
-        }) //resposta sucesso
-        .catch((error:any) => {
-            console.log("Error: nao localiza requesicao " + error);
-            //tesposta un error
-        });
+  function verificarCampoSkill(event: any) {
+    if (event.target.value === "") {
+      // setListaDevsFiltrados(devs); retorno front
+      listarDesenvolvedores(); // retorno da api
 
     }
-    useEffect(() => {listarDesenvolvedores();
+    setSkillDigitado(event.target.value);
+  }
 
-    },[]);
+  function buscarDevPorSkill(event: any) {
+    //não recarrega a pagina
+    event.preventDefault();
 
-    return (
-        <>
-            <main id="main_listadevs">
-                <div className="container container_lista_devs">
-                    <div className="lista_devs_conteudo">
-                        <h1>Lista de Devs</h1>
-                        <hr />
-                        <form method="post" onSubmit={buscarDevPorSkill}>
-                            <div className="wrapper_form">
-                                <label htmlFor="busca">Procurar desenvolvedores</label>
-                                <div className="campo-label">
-                                    <input
-                                        type="search"
-                                        name="campo-busca"
-                                        id="busca"
-                                        placeholder="Buscar desenvolvedores por tecnologias..."
-                                        autoComplete="off"
-                                        onChange={verificarCampoSkill}
-                                    />
-                                    <button type="submit">Buscar</button>
-                                </div>
-                            </div>
-                        </form>
-                        <div className="wrapper_lista">
-                            <ul>
-                                {
-                                    devs.map((dev: any, indice: number) => {
-                                        return <li key={indice}>
-                                            <CardDev
-                                                foto={dev.user_img}
-                                                nome={dev.nome}
-                                                email={dev.email}
-                                                listaTechs={dev.hardskills}
-                                            />
-                                        </li>
-                                    })
-                                }
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </main>
+    //filtrar devs pela skill digitada no campo buscar
+    const devsFiltrados = devs.filter((dev: any) => dev.hardSkills.includes(skillDigitado.toLocaleUpperCase()));
 
-        </>
-    );
+    if (devsFiltrados.length === 0) {
+      alert("Nenhum desenvolvedor(a) com essa skill :(")
+    } else {
+      //atribui valor de devs filtrado, ao state ListaDevsFiltrados 
+      // setListaDevsFiltrados(devsFiltrados);
+      setDevs(devsFiltrados);
+    }
+
+
+  }
+
+  //obter devs da api
+
+  function listarDesenvolvedores() {
+    api.get("users")
+      .then((response: any) => {
+        console.log(response);
+        setDevs(response.data)
+      })
+
+      .catch((error: any) => {
+        console.log("Error ao realizar uma requisição", error);
+
+      })
+
+  }
+
+
+  //monitorar componente - só sera executado dps que a tela carregar
+  useEffect(() => {
+    //executa ação
+    listarDesenvolvedores();
+
+  }, []);
+
+
+  return (
+
+
+    <main id="main_listadevs">
+      <div className="container container_lista_devs">
+        <div className="lista_devs_conteudo">
+          <h1>Lista de Devs</h1>
+          <hr />
+          <form method="post" onSubmit={buscarDevPorSkill}>
+            <div className="wrapper_form">
+              <label htmlFor="busca">Procurar desenvolvedores</label>
+              <div className="campo-label">
+                <input
+                  type="search"
+                  name="campo-busca"
+                  id="busca"
+                  placeholder="Buscar desenvolvedores por tecnologias..."
+                  autoComplete="off"
+                  onChange={verificarCampoSkill}
+                />
+                <button type="submit">Buscar</button>
+              </div>
+            </div>
+          </form>
+          <div className="wrapper_lista">
+            <ul>
+              {
+                devs.map((dev: any, indice: number) => {
+                  return <li key={indice}>
+                    <CardDev
+                      foto={dev.user_img}
+                      nome={dev.nome}
+                      email={dev.email}
+                      listaTechs={dev.hardSkills} />
+                  </li>
+                })
+              }
+            </ul>
+          </div>
+        </div>
+      </div>
+    </main >
+
+
+
+  );
+
 }
-
 export default ListaDevs;
--- vsconnect/src/utils/api.ts
+++ vsconnect/src/utils/api.ts
@@ -1,8 +1,15 @@
-import axios from 'axios';
+ import axios from 'axios';
 
 const api = axios.create({
     baseURL: "http://localhost:3000/"
     
 });
 
-    export default api;
+    export default api; 
+/* import axios from "axios";
+
+const api = axios.create({
+    baseURL: "https://dark-ruby-scallop-robe.cyclic.app/"
+})
+
+export default api; */
